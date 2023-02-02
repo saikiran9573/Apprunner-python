2687b2ce0f83daa67eb85f8889420c5d909e9087
Saikiran <74765192+saikiran9573@users.noreply.github.com>
2023-02-02 20:48:11 +0530

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ def hello_world(request):
     name = os.environ.get('NAME')
     if name == None or len(name) == 0:
         name = "world"
-    message = "Hello, " + name + "!\n"
+    message = "Hello, This is Srisai " + name + "!\n"
     return Response(message)
 
 if __name__ == '__main__':
